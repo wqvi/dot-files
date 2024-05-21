f42bdcb74e35dac50d7465e0e4bad344b2eeae1e
wqvi <137255931+wqvi@users.noreply.github.com>
2024-05-21 09:13:00 -0700

    Set color theme to be catppuccin mocha

--- .bashrc
+++ .bashrc
@@ -18,6 +18,11 @@ export EDITOR='/usr/bin/nvim'
 
 eval "$(zoxide init bash)"
 
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 eval "$(fzf --bash)"
 
 alias vi='/usr/bin/nvim'
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,16 +5,15 @@ vim.cmd('set nu')
 vim.cmd('set rnu')
 vim.opt.autoindent = true
 vim.opt.smarttab = true
--- pywal doesn't work??? :(
---vim.opt.termguicolors = true
 vim.cmd('syntax on')
-vim.cmd('colorscheme vim')
 
 vim.call('plug#begin')
 
 Plug('preservim/nerdtree', { ['on'] = 'NERDTreeToggle' })
+Plug('catppuccin/nvim')
 
 vim.call('plug#end')
 
 vim.cmd('nnoremap <C-e> :NERDTreeToggle<CR>')
 vim.cmd('nnoremap <C-f> :NERDTreeFocus<CR>')
+vim.cmd.colorscheme 'catppuccin-mocha'
